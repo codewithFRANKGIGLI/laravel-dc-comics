dd2344819e362a011f1e57bcbbca1fcb7c0473a2
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-05-24 14:29:21 +0200

    feat: edit btn

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -6,11 +6,12 @@
         <div class="row">
             @foreach ($comics as $comic)
                 <div class="col-4 py-2">
-                    <div class="card" style="width: 18rem; height: 35rem;">
+                    <div class="card" style="width: 18rem; height: 40rem;">
                         <img src="{{ $comic->thumb }}" class="card-img-top" alt="...">
                         <div class="card-body d-flex flex-column justify-content-center">
                             <h5 class="card-title">{{ $comic->title }}</h5>
-                            <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-success">View</a>
+                            <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-success m-2">View</a>
+                            <a href="{{ route('comics.edit', $comic->id) }}" class="btn btn-success m-2">Edit</a>
                         </div>
                     </div>
                 </div>
